5e8486aba9b5dd30983966b20ea4c0b14caa48ba
Takeru Ohta <phjgt308@gmail.com>
2026-01-13 18:22:37 +0900

    feat: implement tokenization in parse_code function

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,20 @@
 pub fn parse_code(text: &str) -> Result<Module, ParseError> {
+    let lexer = erl_tokenize::Lexer::new(text);
+
+    let tokens: Result<Vec<_>, _> = lexer
+        .collect::<Result<Vec<_>, _>>()
+        .map_err(|e| ParseError::Tokenize(e));
+
+    let _tokens = tokens?;
+
+    // TODO: Parse tokens into AST
     todo!()
 }
 
 #[derive(Debug)]
-pub struct ParseError;
+pub enum ParseError {
+    Tokenize(erl_tokenize::Error),
+}
 
 #[derive(Debug)]
 pub struct Module;
