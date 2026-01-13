38740daa7f58e2e611190d39932e5b06e97b3974
Takeru Ohta <phjgt308@gmail.com>
2026-01-13 09:36:21 +0900

    feat: add argument parsing with noargs library

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
-fn main() {
-    println!("Hello, world!");
+fn main() -> noargs::Result<()> {
+    let mut args = noargs::raw_args();
+
+    args.metadata_mut().app_name = env!("CARGO_PKG_NAME");
+    args.metadata_mut().app_description = env!("CARGO_PKG_DESCRIPTION");
+
+    if noargs::VERSION_FLAG.take(&mut args).is_present() {
+        println!("{} {}", env!("CARGO_PKG_NAME"), env!("CARGO_PKG_VERSION"));
+        return Ok(());
+    }
+
+    noargs::HELP_FLAG.take_help(&mut args);
+
+    let name: String = noargs::arg("NAME")
+        .take(&mut args)
+        .then(|a| a.value().parse())?;
+
+    args.finish()?;
+
+    println!("Hello, {}", name);
+
+    Ok(())
 }
