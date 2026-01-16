d70ff6e3aa6ff4733ebce680ee71372d813fee0c
Takeru Ohta <phjgt308@gmail.com>
2026-01-17 02:42:07 +0900

    feat: add JSON conversion for parsed item views

--- src/command_parse.rs
+++ src/command_parse.rs
@@ -18,6 +18,66 @@ pub fn try_run(args: &mut noargs::RawArgs) -> noargs::Result<bool> {
     parser.parse_module()?;
 
     let view = crate::item::ItemView::new(&parser.items, 0);
+    let json_view = to_json(view, &text);
+
+    // Pretty print with formatting
+    let output = nojson::json(|f| {
+        f.set_indent_size(2);
+        f.set_spacing(true);
+        f.value(&json_view.0)
+    });
+
+    println!("{}", output);
 
     Ok(true)
+}
+
+// TODO: rename
+pub fn to_json<'a>(
+    view: crate::item::ItemView<'a>,
+    text: &'a str,
+) -> nojson::Json<ItemViewJson<'a>> {
+    let item_json = ItemViewJson {
+        kind: format!("{:?}", view.kind()),
+        span: (view.span().start, view.span().end),
+        text: view.text(text),
+        children: view
+            .children()
+            .map(|child| to_json(child, text).0)
+            .collect(),
+    };
+    nojson::Json(item_json)
+}
+
+#[derive(Debug)]
+pub struct ItemViewJson<'a> {
+    pub kind: String,
+    pub span: (usize, usize),
+    pub text: &'a str,
+    pub children: Vec<ItemViewJson<'a>>,
+}
+
+impl nojson::DisplayJson for ItemViewJson<'_> {
+    fn fmt(&self, f: &mut nojson::JsonFormatter<'_, '_>) -> std::fmt::Result {
+        f.object(|f| {
+            f.member("kind", &self.kind)?;
+            f.member(
+                "span",
+                nojson::array(|f| {
+                    f.element(self.span.0)?;
+                    f.element(self.span.1)
+                }),
+            )?;
+            f.member("text", self.text)?;
+            f.member(
+                "children",
+                nojson::array(|f| {
+                    for child in &self.children {
+                        f.element(child)?;
+                    }
+                    Ok(())
+                }),
+            )
+        })
+    }
 }
