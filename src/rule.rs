2d5bbb37405df01079c55274a66bb35e8a6e9ac3
Takeru Ohta <phjgt308@gmail.com>
2026-01-17 14:45:53 +0900

    feat: add Context enum and expand RulePattern structure

--- src/rule.rs
+++ src/rule.rs
@@ -1,8 +1,15 @@
+use crate::item::Item;
+
 #[derive(Debug, Clone)]
 pub struct Rule {
     pub title: String,
     pub ng: NgRule,
     pub ok: Option<OkRule>,
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+pub enum Context {
+    Expr,
 }
 
 #[derive(Debug, Clone)]
@@ -22,4 +29,8 @@ pub enum RuleContent {
 }
 
 #[derive(Debug, Clone)]
-pub struct RulePattern {}
+pub struct RulePattern {
+    pub contexts: Vec<Context>,
+    pub items: Vec<Item>,
+    pub comments: Vec<Item>,
+}
