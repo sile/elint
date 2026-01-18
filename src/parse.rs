110b4d69ed83d78fc2f99835297dc624b4e9930b
Takeru Ohta <phjgt308@gmail.com>
2026-01-18 16:07:44 +0900

    feat: implement Display for ParseError with formatted output

--- src/parse.rs
+++ src/parse.rs
@@ -20,8 +20,8 @@ impl ParseError {
 }
 
 impl std::fmt::Display for ParseError {
-    fn fmt(&self, _f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        todo!()
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "Parse Error: {} ({:?})", self.reason, self.span)
     }
 }
 
