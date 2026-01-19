4e034af49031db07d66601ae43dbe8398d84f2b8
Takeru Ohta <phjgt308@gmail.com>
2026-01-19 19:38:30 +0900

    refactor: implement ExpectRules initialization with rule collection

--- src/expect.rs
+++ src/expect.rs
@@ -5,6 +5,8 @@ pub struct ExpectRules {
 
 impl ExpectRules {
     pub fn new(parser: &crate::parse::Parser) -> Result<Self, crate::Error> {
+        let mut rules = Vec::new();
+
         for comment in &parser.comments {
             let text = comment.text(&parser.text);
             let Some(text) = text.strip_prefix("%%").and_then(|t| t.lines().next()) else {
@@ -17,18 +19,33 @@ impl ExpectRules {
             };
             let text = text.trim();
 
+            let Some(target_span) = parser
+                .items
+                .binary_search_by_key(&comment.span, |t| t.span)
+                .err()
+                .and_then(|i| parser.items.get(i).map(|t| t.span))
+            else {
+                continue;
+            };
+
             for name in text.split(',') {
                 let name = name.trim();
-                if !crate::RULE_NAMES.contains(&name) {
+                let Some(rule_name) = crate::RULE_NAMES.iter().find(|v| **v==name) else {
                     return Err(crate::Error::new(
                         comment.span,
                         format!("unknown rule: {name}"),
                     ));
-                }
+                };
+
+                rules.push(ExpectRule {
+                    name: rule_name,
+                    comment_span: comment.span,
+                    target_span,
+                });
             }
-            //
         }
-        todo!()
+
+        Ok(Self { rules })
     }
 }
 
